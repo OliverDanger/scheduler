a8e2303a80ac07fe732e7b00bb72130324963e1a
OliverDanger <od.oliverdixon@gmail.com>
2023-04-22 22:52:04 -0400

    adds selector to get appointments for a day

---
+++ src/helpers/selectors.js
@@ -0,0 +1,10 @@
+export function getAppointmentsForDay(state, day) {
+  const filteredDays = state.days.filter(d => d.name === day);
+  console.log('☀️', filteredDays);
+  const todaysAppointments = (filteredDays.length > 0 ? filteredDays[0].appointments : []);
+  const outputBuffer = []
+  for (const appt of todaysAppointments) {
+    outputBuffer.push(state.appointments[appt])
+  }
+  return outputBuffer;
+}
