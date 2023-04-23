4beb0cc206512e88cee5a77f9308cc636af0557b
OliverDanger <od.oliverdixon@gmail.com>
2023-04-23 15:11:27 -0400

    adds axios api request for days and appointments

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,6 +1,5 @@
 export function getAppointmentsForDay(state, day) {
   const filteredDays = state.days.filter(d => d.name === day);
-  console.log('☀️', filteredDays);
   const todaysAppointments = (filteredDays.length > 0 ? filteredDays[0].appointments : []);
   const outputBuffer = []
   for (const appt of todaysAppointments) {
