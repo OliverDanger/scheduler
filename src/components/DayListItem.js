0ffc44abb766de424a8bcb4d02b9c9528ddcbc8a
OliverDanger <od.oliverdixon@gmail.com>
2023-04-23 00:06:20 +0000

    adds appointment display using test data

---
+++ src/components/DayListItem.js
@@ -0,0 +1,35 @@
+import React from "react";
+import classNames from "classnames";
+
+import "components/DayListItem.scss";
+
+export default function DayListItem(props) {
+
+  const liClass = classNames({
+    "day-list__item": true,
+    "day-list__item--selected": props.selected,
+    "day-list__item--full": props.spots === 0
+  });
+
+  const formatSpots = (spots) => {
+    console.log(spots);
+    if (spots === 0) {
+      return <h3 className="text--light">no spots remaining</h3>
+    }
+    if (spots === 1) {
+      return <h3 className="text--light">1 spot remaining</h3>
+    }
+    return <h3 className="text--light">{spots} spots remaining</h3>
+  };
+
+  return (
+    <li
+      onClick={() => props.setDay(props.name)}
+      className={liClass}
+      selected={props.selected}
+    >
+      <h2 className="text--regular">{props.name}</h2>
+      {formatSpots(props.spots)}
+    </li>
+  );
+}
